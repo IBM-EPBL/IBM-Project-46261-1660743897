966b181d9894f6cc251496fce20c8b7a99a0c5b7
Subasri-7 <116130568+Subasri-7@users.noreply.github.com>
2022-11-23 22:19:33 +0530

    Add files via upload

---
+++ Getting Started With Convolution Neural Network/Start.py
@@ -0,0 +1,4 @@
+# M O D E L    B U I L D I N G
+early_stop_loss = EarlyStopping(monitor='loss', patience=3, verbose=1)
+early_stop_val_acc = EarlyStopping(monitor='val_accuracy', patience=3, verbose=1)
+model_callbacks=[early_stop_loss, early_stop_val_acc]
